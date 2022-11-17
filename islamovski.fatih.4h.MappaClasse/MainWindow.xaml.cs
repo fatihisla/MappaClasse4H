f7cb39553680d1aa5120326d1d476c10cf766960
unknown <fatih.islamovski@studenti.ittsrimini.edu.it>
2022-11-17 10:38:07 +0100

    Prima versione del codice

---
+++ islamovski.fatih.4h.MappaClasse/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace islamovski.fatih._4h.MappaClasse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button  b = sender as Button;
+            MessageBox.Show("banco di :" + b.Tag);
+        }
+      
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di federico casadei");
+        }
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di giovanni morelli");
+        }
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di federico zhang");
+        }
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di islamovski fatih");
+        }
+
+        private void Button_Click5(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di joseph ricci e agostino rossini");
+        }
+        private void Button_Click6(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di matteo tumidei e andrea cancellieri");
+        }
+        private void Button_Click7(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di basilico karol");
+        }
+        private void Button_Click8(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di antonio chietti");
+        }
+        private void Button_Click9(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di leo dirmishi e samuele dervishi");
+        }
+        private void Button_Click10(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di alessandro capanna");
+        }
+        private void Button_Click11(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di federico cau");
+        }
+        private void Button_Click12(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di samuele lombardi");
+        }
+        private void Button_Click13(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di matteo salvi");
+        }
+        private void Button_Click14(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di paolo pompa");
+        }
+        private void Button_Click15(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di francesco priori");
+        }
+        private void Button_Click16(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di davide capicchioni");
+        }
+        private void Button_Click17(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("banco di alessandro burioli");
+        }
+        private void Button_Click18(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("PORTA");
+        }
+        private void Button_Click19(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("CATTEDRA");
+        }
+        private void Button_Click20(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FINESTRA");
+        }
+        private void Button_Click21(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FINESTRA");
+        }
+        private void Button_Click22(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FINESTRA");
+        }
+        private void Button_Click23(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FINESTRA");
+        }
+        private void Button_Click24(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FINESTRA");
+        }
+        private void Button_Click25(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FINESTRA");
+        }
+        private void Button_Click26(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FINESTRA");
+        }
+        private void Button_Click27(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FINESTRA");
+        }
+        private void Button_Click28(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FINESTRA");
+        }
+        private void Button_Click29(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Finestra");
+        }
+        private void Button_Click30(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FINESTRA");
+        }
+        private void Button_Click31(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FINESTRA");
+        }
+
+
+
+
+
+    }
+}
